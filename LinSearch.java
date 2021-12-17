ccb47a134bd3f0ba029a26b16d1797c988c8ac80
William <67127399+thunder-red-star@users.noreply.github.com>
2021-12-17 11:59:00 -0500

    Update LinSearch.java

--- LinSearch.java
+++ LinSearch.java
@@ -1,1 +1,26 @@
+/**
+ class LinSearch
+ **/
 
+public class LinSearch
+{
+    public static int linSearch ( Comparable[] a, Comparable target ) {
+        int i = 0;
+        while (i < a.length) {
+            if (a[i] == target) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    private static void toString( Object[] arr )
+    {
+        String output = "[";
+        for( Object o : arr )
+            output += o + ", ";
+        output = output.substring( 0, output.length()-2 ) + "]";
+        System.out.println( output );
+    }
+}
