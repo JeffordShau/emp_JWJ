eb70f50a61ecba67d54d0097ed2da9b875a42f6e
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-12-19 01:47:36 -0500

    Update LinSearch.java

--- LinSearch.java
+++ LinSearch.java
@@ -1,28 +1,41 @@
 /**
- class LinSearch
- **/
-
-import java.Utils.ArrayList; 
+   class LinSearch
+**/
 
 public class LinSearch
 {
-    public static int linSearch ( ArrayList a, int target ) {
-        int i = 0;
-        while (i < a.length) {
-            if (a[i],compareTo(target)) {
-                return i;
-            }
-            i++;
-        }
-        return -1;
-    }
 
-    private static void toString( Object[] arr )
-    {
-        String output = "[";
-        for( Object o : arr )
-            output += o + ", ";
-        output = output.substring( 0, output.length()-2 ) + "]";
-        System.out.println( output );
+  /**
+     int linSearch(Comparable[],Comparable) -- searches an array of
+     Comparables for target
+     post: returns index of first occurrence of target, or
+     returns -1 if target not found
+  **/
+  public static int linSearch ( Comparable[] a, Comparable target )
+  {
+    int tPos = -1;
+    int i = 0;
+
+    while ( i < a.length ) {
+      if (a[i] == target) {
+        tPos = i;
+        return tPos;
+      }
+      i += 1;
     }
-}
+    return tPos;
+  }
+
+
+  // utility/helper fxn to display contents of an array of Objects
+  private static void toString( Object[] arr )
+  {
+    String output = "[ ";
+
+    for( Object o : arr )
+      output += o + ", ";
+
+    output = output.substring( 0, output.length()-2 ) + " ]";
+
+    System.out.println( output );
+  }
